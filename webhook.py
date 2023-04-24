7bc5e4dbc8b1bc90e29d29aab48f89497d47be66
bikmetle <hamid-nv@vivaldi.net>
2023-04-24 15:50:35 +0300

    bach webhookpath

--- webhook.py
+++ webhook.py
@@ -8,7 +8,7 @@ from config import TOKEN, OWNER, DEV
 
 API_TOKEN = TOKEN
 WEBHOOK_HOST = 'https://testwebhookheroku.herokuapp.com/'
-WEBHOOK_PATH = ''
+WEBHOOK_PATH = f'/{TOKEN}'
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = int(os.environ.get('PORT', '8443'))
@@ -38,7 +38,7 @@ async def echo_message(msg: types.Message):
 if __name__ == '__main__':
     executor.start_webhook(
         dispatcher=dp,
-        webhook_path=WEBHOOK_URL,
+        webhook_path=WEBHOOK_PATH,
         on_startup=on_startup,
         on_shutdown=on_shutdown,
         skip_updates=True,
