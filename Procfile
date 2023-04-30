94f48cd59abe7c15caee90bfc5ecfca6144dc810
bikmetle <hamid-nv@vivaldi.net>
2023-05-01 00:26:12 +0300

    test for paw

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 greeting_bot.py
+worker: python3 greeting_bot.py
--- greeting_bot.py
+++ greeting_bot.py
@@ -7,8 +7,10 @@ from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from sql_model import MessageModel, Session
-from config import TOKEN, OWNER, DEV, WEBHOOK_HOST, WEBHOOK_PATH, WEBAPP_HOST
-from sqlalchemy import func
+from config import TOKEN, OWNER, DEV, WEBHOOK_HOST, WEBHOOK_PATH, WEBAPP_HOST, PRODUCTION
+from flask import Flask
+
+app = Flask(__name__)
 
 
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
@@ -19,6 +21,17 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+@app.route("/webhook", methods=["POST"])
+async def webhook(request):
+    update = types.Update.de_json(request.json, bot)
+    await dp.process_update(update)
+    return "OK"
 
 
 def ad_id_and_text(text):
@@ -79,19 +92,22 @@ def captcha(member):
 async def process_right_query(query: types.CallbackQuery):
     _, member_id = query.data.split()
     user_id = query.from_user.id
+    full_name = query.from_user.full_name
     chat_id = query.message.chat.id
+    message_id = query.message.message_id
     if user_id == int(member_id):
-        text = f"<a href='tg://user?id={user_id}'>{query.from_user.full_name}</a> добро пожаловать в наш чат!\n" \
+        text = f"<a href='tg://user?id={user_id}'>{full_name}</a> добро пожаловать в наш чат!\n" \
             "📌Просьба соблюдать простые правила в закрепленных сообщениях👆\n\n" \
             "ВНЖ. НОТАРИУС @Kaganatski\nБайер. Золото @istanbuldan_kyz\nЧартерные билеты. Туры @venuspower\n" \
             "Недвижимость @anara_realestate\nПо поводу рекламы писать @Kaganatski"
-        await query.message.delete()
-        permissions = types.ChatPermissions(can_send_messages=True, can_send_media_messages=True)
-        await query.message.bot.restrict_chat_member(
-            chat_id,
-            user_id,
-            permissions
-        )
+        await bot.delete_message(chat_id, message_id)
+        # permissions = types.ChatPermissions(can_send_messages=True, can_send_media_messages=True)
+        await bot.promote_chat_member(chat_id, user_id)
+        # await query.message.bot.restrict_chat_member(
+        #     chat_id,
+        #     user_id,
+        #     permissions
+        # )
         message = await bot.send_message(chat_id=chat_id, text=text, parse_mode="HTML")
         await asyncio.sleep(30)
         await message.delete()
@@ -109,13 +125,11 @@ async def process_wrong_query(query: types.CallbackQuery):
 
 async def on_startup(dp):
     await bot.set_webhook(WEBHOOK_URL)
-    await bot.send_message(chat_id=OWNER, text='Bot has been started')
     await bot.send_message(chat_id=DEV, text='Bot has been started')
 
 
 async def on_shutdown(dp):
     logging.warning('Shutting down..')
-    await bot.send_message(chat_id=OWNER, text='Bot has been stopped')
     await bot.send_message(chat_id=DEV, text='Bot has been stopped')
     logging.warning('Bye!')
 
@@ -141,18 +155,22 @@ async def status_msg(message: types.Message):
             obj = session.query(MessageModel).filter_by(id=ad_id).first()
             user_link = get_user_link_2(obj.user_id, obj.full_name, obj.username)
             text=f'[{obj.id}].\n{user_link}{obj.text}\nосталось {obj.count}'
-            await bot.send_message(chat_id=OWNER, text=text, parse_mode="HTML")
-            await bot.send_message(chat_id=DEV, text=text, parse_mode="HTML")
+            await message.answer(text=text, parse_mode="HTML")
+            await message.delete()
+            await message.reply_to_message.delete()
+
         except:
-            ad_id, _ = ad_id_and_text(message.reply_to_message.caption)
-            obj = session.query(MessageModel).filter_by(id=ad_id).first()
-            user_link = get_user_link_2(obj.user_id, obj.full_name, obj.username)
-            photo=message.reply_to_message.photo[-1].file_id
-            caption=f'[{obj.id}].\n{user_link}{obj.text if obj.text else ""}\nосталось {obj.count}'
-            await bot.send_photo(chat_id=OWNER, photo=photo, caption=caption, parse_mode="HTML")
-            await bot.send_photo(chat_id=DEV, photo=photo, caption=caption, parse_mode="HTML")
-        await message.delete()
-        await message.reply_to_message.delete()
+            try:
+                ad_id, _ = ad_id_and_text(message.reply_to_message.caption)
+                obj = session.query(MessageModel).filter_by(id=ad_id).first()
+                user_link = get_user_link_2(obj.user_id, obj.full_name, obj.username)
+                photo=message.reply_to_message.photo[-1].file_id
+                caption=f'[{obj.id}].\n{user_link}{obj.text if obj.text else ""}\nосталось {obj.count}'
+                await message.answer_photo(photo=photo, caption=caption, parse_mode="HTML")
+                await message.delete()
+                await message.reply_to_message.delete()
+            except:
+                await message.answer(text='No data')
 
 
 @dp.message_handler(commands=['spam', 'спам'])
@@ -167,6 +185,7 @@ async def spam_msg(message: types.Message):
         session = Session()
         session.query(MessageModel).filter_by(id=ad_id).update({'count': -1})
         session.commit()
+        await bot.delete_message(chat_id=-1001374520594, message_id=ad_id)
         await message.delete()
 
 
@@ -176,7 +195,7 @@ async def join_group(updated: types.ChatMemberUpdated):
     Restrict sending messages for new group members
     """
     try:
-        if not updated.old_chat_member.is_member:
+        if updated.old_chat_member.status == 'left' and updated.new_chat_member.is_chat_member():
             chat_id = updated.chat.id
             permissions = types.ChatPermissions(can_send_messages=False, can_send_media_messages=False)
             await updated.bot.restrict_chat_member(
@@ -187,11 +206,30 @@ async def join_group(updated: types.ChatMemberUpdated):
             text, keyboard = captcha(updated.from_user)
             greeting = await bot.send_message(chat_id=chat_id, text=text, reply_markup=keyboard, parse_mode="HTML")
             await asyncio.sleep(30)
-            if not updated.new_chat_member.can_send_messages:
+            try:
                 await greeting.delete()
+            except:
+                pass
+        elif updated.new_chat_member.status == 'restricted':
+            if updated.old_chat_member.status == 'member':
+                pass
+            elif updated.old_chat_member.is_member is False and updated.new_chat_member.is_member:
+                chat_id = updated.chat.id
+                permissions = types.ChatPermissions(can_send_messages=False, can_send_media_messages=False)
+                await updated.bot.restrict_chat_member(
+                    chat_id,
+                    updated.from_user.id,
+                    permissions
+                )
+                text, keyboard = captcha(updated.from_user)
+                greeting = await bot.send_message(chat_id=chat_id, text=text, reply_markup=keyboard, parse_mode="HTML")
+                await asyncio.sleep(30)
+                try:
+                    await greeting.delete()
+                except:
+                    pass
 
     except Exception as e:
-        # await bot.send_message(chat_id=OWNER, text=f'join_group {e}')
         await bot.send_message(chat_id=DEV, text=f'join_group {e}\n{updated}')
     
 
@@ -214,14 +252,28 @@ async def handle_message(message: types.Message):
             session.query(MessageModel).filter_by(id=obj.id).update({'count': obj.count-1})
             session.commit()
         elif obj.count < 0:
+            await bot.delete_message(chat_id=chat_id, message_id=message_id)
             session.query(MessageModel).filter_by(id=obj.id).update({'count': obj.count-1})
             session.commit()
-            await bot.delete_message(chat_id=chat_id, message_id=message_id)
         else:
-            text=f"[{obj.id}].\n{user_link}{text}\nосталось {obj.count}"
-            await bot.send_message(chat_id=OWNER, text=text, parse_mode="HTML")
-            await bot.send_message(chat_id=DEV, text=text, parse_mode="HTML")
-            # await bot.delete_message(chat_id=chat_id, message_id=message_id)
+            msg=f"[{message_id}].\n{user_link}{text}\nосталось {obj.count}"
+            await bot.send_message(chat_id=OWNER, text=msg, parse_mode="HTML")
+            await bot.send_message(chat_id=DEV, text=msg, parse_mode="HTML")
+            try:
+                await bot.delete_message(chat_id=chat_id, message_id=obj.id)
+            except:
+                print('text already deleted')
+            db_message = MessageModel(
+                id = message_id,
+                user_id=user_id,
+                full_name=full_name,
+                username=username,
+                text=text,
+                date=date
+            )
+            session.add(db_message)
+            session.query(MessageModel).filter_by(id=obj.id).delete()
+            session.commit()
     except:
         db_message = MessageModel(
             id = message_id,
@@ -250,7 +302,7 @@ async def handle_photo(message: types.Message):
     user_id = message.from_user.id
     full_name = message.from_user.full_name
     username = message.from_user.username
-    file_id = message.photo[-1]['file_unique_id']
+    file_id = message.photo[-1].file_unique_id
     caption = message.caption
     date = message.date
     user_link = get_user_link(message)
@@ -258,15 +310,34 @@ async def handle_photo(message: types.Message):
     session = Session()
     obj = session.query(MessageModel).filter_by(file_id=file_id).first()
     try:
-        if obj.count:
+        if obj.count > 0:
+            session.query(MessageModel).filter_by(id=obj.id).update({'count': obj.count-1})
+            session.commit()
+        elif obj.count < 0:
+            await bot.delete_message(chat_id=chat_id, message_id=message_id)
             session.query(MessageModel).filter_by(id=obj.id).update({'count': obj.count-1})
             session.commit()
         else:
             photo = message.photo[-1].file_id
-            caption = f'[{obj.id}].\n{user_link}{caption if caption else ""}\nосталсь {obj.count}'
+            caption = f'[{message_id}].\n{user_link}{caption if caption else ""}\nосталсь {obj.count}'
             await bot.send_photo(chat_id=OWNER, photo=photo, caption=caption, parse_mode="HTML")
             await bot.send_photo(chat_id=DEV, photo=photo, caption=caption, parse_mode="HTML")
-            await bot.delete_message(chat_id=chat_id, message_id=message_id)
+            try:
+                await bot.delete_message(chat_id=chat_id, message_id=obj.id)
+            except:
+                print("photo already deleted")
+            db_message = MessageModel(
+                id = message_id,
+                user_id=user_id,
+                full_name=full_name,
+                username=username,
+                text=caption,
+                file_id=file_id,
+                date=date
+            )
+            session.add(db_message)
+            session.query(MessageModel).filter_by(id=obj.id).delete()
+            session.commit()
     except:
         db_message = MessageModel(
             id = message_id,
@@ -301,34 +372,39 @@ async def manage_count(message: types.Message):
                 obj = session.query(MessageModel).filter_by(id=ad_id).first()
                 user_link = get_user_link_2(obj.user_id, obj.full_name, obj.username)
                 text=f'[{obj.id}].\n{user_link}{obj.text}\nосталось {new_count}'
-                await bot.send_message(chat_id=OWNER, text=text, parse_mode="HTML")
-                await bot.send_message(chat_id=DEV, text=text, parse_mode="HTML")
+                await message.answer(text=text, parse_mode="HTML")
+                session.query(MessageModel).filter_by(id=ad_id).update({'count': new_count})
+                session.commit()
+                await message.delete()
+                await message.reply_to_message.delete()
             except:
-                ad_id, _ = ad_id_and_text(message.reply_to_message.caption)
-                obj = session.query(MessageModel).filter_by(id=ad_id).first()
-                user_link = get_user_link_2(obj.user_id, obj.full_name, obj.username)
-                photo = message.reply_to_message.photo[-1].file_id
-                caption = f'[{obj.id}].\n{user_link}{obj.text if obj.text else ""}\nосталось {new_count}'
-                await bot.send_photo(chat_id=OWNER, photo=photo, caption=caption, parse_mode="HTML")
-                await bot.send_photo(chat_id=DEV, photo=photo, caption=caption, parse_mode="HTML")
-            session.query(MessageModel).filter_by(id=ad_id).update({'count': new_count})
-            session.commit()
-            await message.delete()
+                try:
+                    ad_id, _ = ad_id_and_text(message.reply_to_message.caption)
+                    obj = session.query(MessageModel).filter_by(id=ad_id).first()
+                    user_link = get_user_link_2(obj.user_id, obj.full_name, obj.username)
+                    photo = message.reply_to_message.photo[-1].file_id
+                    caption = f'[{obj.id}].\n{user_link}{obj.text if obj.text else ""}\nосталось {new_count}'
+                    await message.answer_photo(photo=photo, caption=caption, parse_mode="HTML")
+                    session.query(MessageModel).filter_by(id=ad_id).update({'count': new_count})
+                    session.commit()
+                    await message.delete()
+                    await message.reply_to_message.delete()
+                except:
+                    await message.answer(text='No data')
         except:
             await message.answer(text='reply to ad msg please')
 
 
-# if __name__ == '__main__':
-#     executor.start_webhook(
-#         dispatcher=dp,
-#         webhook_path=WEBHOOK_PATH,
-#         on_startup=on_startup,
-#         on_shutdown=on_shutdown,
-#         skip_updates=True,
-#         host=WEBAPP_HOST,
-#         port=WEBAPP_PORT,
-#     )
-
-
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    if PRODUCTION:
+        executor.start_webhook(
+            dispatcher=dp,
+            webhook_path=WEBHOOK_PATH,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            skip_updates=True,
+            host=WEBAPP_HOST,
+            port=WEBAPP_PORT,
+        )
+    else:
+        executor.start_polling(dp)
